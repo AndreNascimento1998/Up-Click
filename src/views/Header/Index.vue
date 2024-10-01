d969da24b83b790f4e0cfa6cb95eb1eb12d13f9b
Andre <andre.ncardoso@hotmail.com>
2024-10-01 17:30:23 -0300

    feat: create Header

---
+++ src/views/Header/Index.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import Logo from '@/assets/icons/Logo/Index.vue'
+import Dropdown from "@/components/base/Dropdown/Dropdown.vue";
+
+const optionsDropdown = [
+    {
+        name: 'The Action 1st',
+        action: () => console.log('The Action 1st')
+    },
+    {
+        name: 'Sair',
+        action: () => logout()
+    }
+]
+
+const logout = () => {
+    console.log('Logout')
+}
+
+</script>
+
+<template>
+    <main class="container-header">
+        <Logo />
+        <article class="container-header_">
+            <Dropdown user-name="juka" :options="optionsDropdown" />
+        </article>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+.container-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: $bg-secondary;
+    padding: 1rem;
+
+    @media (min-width: $xl) {
+        padding: 2rem 10rem;
+    }
+}
+</style>
