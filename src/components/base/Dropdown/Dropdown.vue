6609bf245bd6535b65c9457b5c8066c251e49a3d
Andre <andre.ncardoso@hotmail.com>
2024-10-01 17:29:33 -0300

    feat: create component generic, and documentation

---
+++ src/components/base/Dropdown/Dropdown.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+
+import {computed} from "vue";
+
+const props = defineProps({
+    options: {
+        type: Array,
+        default: () => [],
+        required: true
+    },
+    userName: {
+        type: String,
+        default: '',
+        required: true
+    }
+})
+
+const nameParsed = computed(() => {
+    return props.userName.split('')[0].toUpperCase()
+})
+</script>
+
+<template>
+    <main class="container-dropdown">
+        <el-dropdown placement="top-start">
+            <el-avatar class="container-dropdown_avatar">
+                <span class="container-dropdown_avatar_text">
+                    {{ nameParsed }}
+                </span>
+            </el-avatar>
+            <template #dropdown>
+                <el-dropdown-menu >
+                    <el-dropdown-item
+                        v-for="(option, index) in options"
+                        :key="index"
+                        @click="option.action"
+                    >
+                        {{ option.name }}
+                    </el-dropdown-item>
+                </el-dropdown-menu>
+            </template>
+        </el-dropdown>
+    </main>
+</template>
+
+<style scoped lang="scss">
+.container-dropdown {
+    width: 100%;
+
+    &_avatar {
+        width: 60px;
+        height: 60px;
+        background-color: $title-color;
+        cursor: pointer;
+
+        &_text {
+            font-size: 30px;
+        }
+    }
+}
+</style>
