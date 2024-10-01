0f2c104de657586af6831a91d7e8ffb9ce0b52d3
Andre <andre.ncardoso@hotmail.com>
2024-10-01 11:16:03 -0300

    feat: create component generic

---
+++ src/components/base/Button/Button.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+
+const emits = defineEmits(['click'])
+
+const props = defineProps({
+    text: {
+        type: String,
+        default: 'Button',
+        required: true
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+        required: false
+    },
+    type: {
+        type: String,
+        default: 'Button',
+        required: false
+    },
+    nativeType: {
+        type: String,
+        default: 'button',
+        required: false
+    }
+})
+</script>
+
+<template>
+    <main>
+        <el-button
+            @click="() => emits('click')"
+            type="primary"
+            :native-type="nativeType"
+            style="width: 100%"
+            :disabled="disabled"
+        >
+            {{ text }}
+        </el-button>
+    </main>
+</template>
