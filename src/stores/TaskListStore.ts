a26d57e6e91b41c5fe82f3770e19760fb9b92195
Andre <andre.ncardoso@hotmail.com>
2024-10-05 12:57:12 -0300

    feat: create get task

--- src/stores/TaskListStore.ts
+++ src/stores/TaskListStore.ts
@@ -1,11 +1,45 @@
 import {defineStore} from "pinia"
 import ITaskList from "@/types/ITaskList.ts"
 import { auth } from '@/firebaseConfig.ts'
-import { getDatabase, push, ref, set } from "firebase/database"
+import { getDatabase, push, ref, set, get } from "firebase/database"
 
 export const TaskListStore = defineStore('taskList', () => {
 
     const db = getDatabase()
+
+    async function getTasks() {
+        const user = auth.currentUser
+
+        if (user) {
+            const taskListRef = ref(db, `tasks/${user?.uid}`)
+
+            try {
+                const snapshot = await get(taskListRef)
+
+                if (snapshot.exists()) {
+                    const tasks = snapshot.val()
+
+                    const taskList = Object.keys(tasks).map(key => ({
+                        id: key,
+                        ...tasks[key]
+                    }))
+
+                    console.log(taskList)
+
+                    return taskList
+                } else {
+                    console.log('No data available')
+                    return []
+                }
+            } catch (e) {
+                console.error('Nenhuma tarefa encontrada', e)
+                return []
+            }
+        } else {
+            console.log('Usuário não está logado')
+            return []
+        }
+    }
 
     async function addTask(form: ITaskList) {
         const user = auth.currentUser
@@ -31,6 +65,7 @@ export const TaskListStore = defineStore('taskList', () => {
     }
 
     return {
-        addTask
+        addTask,
+        getTasks
     }
 })
