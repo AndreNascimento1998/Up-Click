0322ac12a2590d77211dd474d1858f41a117e73e
Andre <andre.ncardoso@hotmail.com>
2024-10-05 21:26:38 -0300

    feat: create component generic

---
+++ src/components/base/Tooltip/Tooltip.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+const props = defineProps<{
+   text: string
+}>()
+</script>
+
+<template>
+    <el-tooltip placement="top">
+        <template #content>
+            <span style="font-size: 18px">{{props.text}}</span>
+        </template>
+        <slot/>
+    </el-tooltip>
+</template>
